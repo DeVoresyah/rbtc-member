4c392d6fb98a2a7919256998d3ac0212a24407ef
DeVoresyah <de.voresyah@gmail.com>
2017-02-28 23:03:25 +0700

    First Commit

---
+++ js/chart-data.js
@@ -0,0 +1,51 @@
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var doughnut = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+window.onload = function() {
+    var ctx = document.getElementById("line-chart").getContext("2d");
+    var ctx2 = document.getElementById("doughnut-chart").getContext("2d");
+    var myLineChart = new Chart(ctx).Line(data, {responsive:true});
+    var myDoughnutChart = new Chart(ctx2).Doughnut(doughnut,{responsive:true});
+}
